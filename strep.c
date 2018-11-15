1ab704a5a7f17491278f727da3bcb09cdcdaba9e
Puneet <pjohal@stuy.edu>
2018-11-15 05:03:05 -0500

    testing counter

--- strep.c
+++ strep.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 
+//helper: returns # of args in line
 int counter(char * line){
   int i = 0;
   int count = 0;
@@ -10,15 +11,15 @@ int counter(char * line){
     count += (line[i] == ' ');
     i++;
   }
-  return count;
+  return count + 1;
 }
 
 char ** parse_args( char * line ){
   char ** str = (char **) malloc(200);
   char *s1 = line;
   int i;
-  int flags = counter(line) + 1;
-  //printf("%d\n", flags);
+  int flags = counter(line);
+  printf("Num of flags: %d\n", flags);
   for (i = 0; i < flags; i++){
     // printf("DOING STRSEP [%s]\n", strsep( &s1, " " ));
     // printf("The Addition:%s\n", s1);
@@ -39,10 +40,10 @@ void printer(char ** line, int flags){
 int main(int argc, char * argv[]){
     char command[100];
     printf("Type in command: ");
-    //scanf("%s", command);
-    fgets(command, 20, stdin);
+    scanf("%s", command);
+    //fgets(command, 20, stdin);
     char ** args = parse_args(command);
-    printer(args, counter(command)+1);
+    printer(args, counter(command));
     //executing the command (args[0]) with the flags (args)
     // printf("ARGS[0]: %s\n", args[0]);
     execvp(args[0], args);
