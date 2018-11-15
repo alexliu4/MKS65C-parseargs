90433d6121ad93c3685bb74fea8ea84a1ea3b584
Puneet <pjohal@stuy.edu>
2018-11-15 06:07:31 -0500

    got rid of flags and counter

--- strep.c
+++ strep.c
@@ -7,7 +7,7 @@
 int counter(char * line){
   int count = 1, len = strlen(line), i;
   for (i=0; i<=len; i++){
-    if (line[i] ==' '){
+    if (line[i] == ' '){
       count++;
     }
   }
@@ -15,23 +15,23 @@ int counter(char * line){
 }
 
 char ** parse_args( char * line ){
-  char ** str = (char **) malloc(200);
-  char *s1 = line;
+  char ** str = malloc(sizeOf(char *) * 6);
+  char * s1 = line;
   int i;
-  int flags = counter(line);
+  //int flags = counter(line);
   //printf("Num of flags: %d\n", flags);
-  for (i = 0; i < flags; i++){
+  for (i = 0; i < 6; i++){
     // printf("DOING STRSEP [%s]\n", strsep( &s1, " " ));
     // printf("The Addition:%s\n", s1);
-    str[i] = strsep( &s1, " " );
+    str[i] = strsep( &s1, " ");
     //printf("[%s]\n", s1);
   }
   return str;
 }
 
-void printer(char ** line, int flags){
+void printer(char ** line){
   int i = 0;
-  while (i < flags){
+  while (i < 6){
     printf("The %dth element: %s\n",i , line[i]);
     i++;
   }
@@ -41,10 +41,10 @@ int main(int argc, char * argv[]){
     char command[100];
     printf("Type in command: ");
     scanf("%s", command);
-    printf("Num of args: %d\n", counter(command));
+    //printf("Num of args: %d\n", counter(command));
     //fgets(command, 20, stdin);
     char ** args = parse_args(command);
-    printer(args, counter(command));
+    printer(args);
     //executing the command (args[0]) with the flags (args)
     // printf("ARGS[0]: %s\n", args[0]);
     execvp(args[0], args);
