374f4fe4c7f1c7cee099e4821868f63416205c1a
alexliu4 <aliu9@stuy.edu>
2018-11-16 01:19:19 -0500

    fixing

--- strep.c
+++ strep.c
@@ -2,54 +2,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-//helper: returns # of args in line
+int flags = 0;
+
 int counter(char * line){
-  int count = 1, len = strlen(line), i;
-  for (i=0; i<=len; i++){
-    if (line[i] == ' '){
-      count++;
-    }
+  int i, count;
+  for (i = 0, count = 0; line[i]; i++){
+    count += (line[i] == ' ');
   }
   return count;
 }
 
 char ** parse_args( char * line ){
-  char ** str = malloc(sizeof(char *) * 6);
-  char * s1 = line;
+  char ** str = (char **) malloc(4);
+  char *s1 = line;
   int i;
-  //int flags = counter(line);
-  //printf("Num of flags: %d\n", flags);
-  for (i = 0; i < 6; i++){
+  flags = counter(line) + 1;
+  //printf("%d\n", flags);
+  for (i = 0; i < flags; i++){
     // printf("DOING STRSEP [%s]\n", strsep( &s1, " " ));
     // printf("The Addition:%s\n", s1);
-    str[i] = strsep( &s1, " ");
+    str[i] = strsep( &s1, " " );
     //printf("[%s]\n", s1);
   }
   return str;
 }
 
-//helper: prints command + args
 void printer(char ** line){
   int i = 0;
-  while (i < 6){
-    printf("The %dth element: %s\n", i, line[i]);
+  while (i < flags){
+    printf("The %dth element: %s\n",i , line[i]);
     i++;
   }
 }
 
 int main(int argc, char * argv[]){
-  char command[100] = "la -a -l";
-  printf("Type in command: %s\n", command);
-  //scanf("%s", command);
-  //printf("Num of args: %d\n", counter(command));
-  //fgets(command, 20, stdin);
-  char ** args = parse_args(command);
-  printer(args);
-  //executing the command (args[0]) with the flags (args)
-  // printf("ARGS[0]: %s\n", args[0]);
-  printf ("Running execvp...\n");
-  execvp(args[0], args);
-  free(args);
+    char command[100];
+    printf("Type in command: ");
+    // scanf("%s", command);
+    fgets(command, 100, stdin);
+    char ** args = parse_args(command);
+    printer( args );
+    //executing the command (args[0]) with the flags (args)
+    // printf("ARGS[0]: %s\n", args[0]);
+    // char * ans = strcat("/bin/", args[0]);
+    // execvp(args[0], args);
+    // printf("ERROR: %s\n", strerror(errno));
   return 0;
 }
